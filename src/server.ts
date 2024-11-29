5fdc3c2ba7c95132fa6502625ff486b0ac043707
Gabriel Pink <aws@gabriel-rosa.com>
2024-11-29 00:24:08 -0300

    🚧 ip address test

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import { IncomingMessage } from 'http';
 import { WebSocketServer, WebSocket } from 'ws';
 
 const PORT = 8080;
@@ -5,21 +6,28 @@ const PORT = 8080;
 // Inicializa o servidor WebSocket
 const wss = new WebSocketServer({ port: PORT });
 
-wss.on('connection', (ws: WebSocket) => {
-  console.log('Cliente conectado!');
+wss.on('connection', (ws: WebSocket, request: IncomingMessage) => {
+    console.log('Cliente conectado!');
 
-  // Recebe mensagens do cliente
-  ws.on('message', (message: string) => {
+    console.log({
+        ip: request.socket.remoteAddress,
+        userAgent: request.headers['user-agent'],
+        entryTime: Date.now(),
+        actions: []
+    })
+
+    // Recebe mensagens do cliente
+    ws.on('message', (message: string) => {
     console.log(`Mensagem recebida: ${message}`);
 
     // Responde ao cliente
     ws.send(`Mensagem recebida: ${message}`);
-  });
+    });
 
-  // Lida com o fechamento da conexão
-  ws.on('close', () => {
+    // Lida com o fechamento da conexão
+    ws.on('close', () => {
     console.log('Cliente desconectado.');
-  });
+    });
 });
 
 console.log(`Servidor WebSocket rodando na porta ${PORT}`);
