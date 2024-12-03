2c893f354b5f1bcc9cbb6017a4936f25c01ffdf7
Gabriel Pink <aws@gabriel-rosa.com>
2024-12-03 05:12:19 -0300

    🎛️ Add controllers in src/controllers/

---
+++ src/controllers/connectionController.ts
@@ -0,0 +1,47 @@
+import { IncomingMessage } from "http";
+import { v4 as uuidv4 } from "uuid";
+import { authenticateAdmin } from "../services/authService";
+import { logConnection, logError } from "../services/loggerService";
+import { sendToAllAdmins } from "../utils/websocketUtils";
+import { ClientConnection } from "../models/clienteConnection";
+import { ClientData } from "../models/clientData";
+import { getSerializableUserClients } from "../utils/getSerializableUserClients";
+
+export const handleConnection = (ws: ClientConnection, request: IncomingMessage, adminClients: any, userClients: any) => {
+    const connectionId = uuidv4();
+    ws.connectionId = connectionId;
+
+    if (request.headers.authorization) {
+        const token = request.headers.authorization.split(' ')[1];
+
+        try {
+
+            authenticateAdmin(token);
+            ws.role = 'admin';
+            adminClients.add(ws);
+            logConnection(`Admin from ${request.socket.remoteAddress} connected.`);
+            ws.send(JSON.stringify({ type: 'USER_LIST', data: getSerializableUserClients(userClients) }));
+
+        } catch (error) {
+
+            logError('[*] Login attempt by an admin, connection refused');
+            ws.close();
+
+        }
+    } else {
+
+        ws.role = 'user';
+
+        const clientData: ClientData = {
+            ip: request.socket.remoteAddress,
+            userAgent: request.headers['user-agent'],
+            entryTime: new Date(),
+            userActivities: [],
+        };
+
+        userClients.set(connectionId, clientData);
+        
+        logConnection(`User from ${request.socket.remoteAddress} connected.`);
+        sendToAllAdmins(adminClients, JSON.stringify({ type: 'CONN_RECV', data: clientData }));
+    }
+};
---
+++ src/controllers/messageController.ts
@@ -0,0 +1,30 @@
+import { ClientConnection } from '../models/clienteConnection';
+import { addUserActivity } from '../services/userService';
+import { ClientData } from '../models/clientData';
+
+export const handleApplicationMessages = (ws: ClientConnection, parsedData: any, adminClients: Set<ClientConnection>, userClients: Map<string, ClientData>) => {
+
+    if (ws.role !== 'user') {
+        ws.send(JSON.stringify({ type: 'ERROR', message: 'Only users can send LOG messages.' }));
+        return;
+    }
+
+    if (parsedData.type === 'LOG') {
+        
+        console.log(`Log received from user: ${ws.connectionId}`);
+        const clientData = userClients.get(ws.connectionId);
+        const { content, actionType } = parsedData.data;
+
+        if (clientData) {
+            addUserActivity(clientData, content, actionType);
+        }
+
+        adminClients.forEach((admin) => {
+            admin.send(JSON.stringify({ ...parsedData, from: ws.connectionId }));
+        });
+    } else {
+
+        ws.send(JSON.stringify({ type: 'ERROR', message: 'Invalid message type or format.' }));
+
+    }
+};
