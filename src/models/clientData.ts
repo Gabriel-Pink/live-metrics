e32753f2fbeef2afbb86bf9e05a5b04c3f515294
Gabriel Pink <aws@gabriel-rosa.com>
2024-12-04 00:50:31 -0300

    📊 Update src/models/clientData.ts

--- src/models/clientData.ts
+++ src/models/clientData.ts
@@ -3,7 +3,7 @@ import { UserActivities } from './userActivities';
 export interface ClientData {
   ip: string | undefined;
   userAgent: string | undefined;
-  entryTime: Date;
-  exitTime?: Date;
+  entryTime: number;
+  exitTime?: number;
   userActivities: UserActivities[];
 }
