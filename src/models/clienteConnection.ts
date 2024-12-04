e9a6bf7cc39b496fb59cb5ea362c697e3c0e7816
Gabriel Pink <aws@gabriel-rosa.com>
2024-12-04 00:52:09 -0300

    ❌ Delete src/models/clienteConnection.ts

--- src/models/clienteConnection.ts
+++
@@ -1,9 +0,0 @@
-import WebSocket from "ws";
-import { Role } from "./role";
-import { ClientData } from "./clientData";
-
-export interface ClientConnection extends WebSocket {
-    role: Role;
-    connectionId: string;
-    data: ClientData;
-}
